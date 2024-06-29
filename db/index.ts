d548b836b9d174560074c2dec8b02a74fa3fcc10
Sanya Hussain <sanz_133@hotmail.com>
2024-06-29 18:50:06 +0100

    deleted index file

--- db/index.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const db = mongoose.connection;
-
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
--- dist/db/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const db = mongoose_1.default.connection;
-mongoose_1.default.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
