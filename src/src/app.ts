2dbbf9adba5fe6fe26af79ee4296837f067f4386
Sanya Hussain <sanz_133@hotmail.com>
2024-06-30 14:29:49 +0100

    connected app to db

--- src/src/app.ts
+++ src/src/app.ts
@@ -1,9 +1,13 @@
 const express = require('express');
+import connectMongoose from "./connect";
+
 const app = express();
 
 app.use(express.json());
 
+connectMongoose()
+
 app.get('/api/users', (req: any, res: any, next: any) => {
-  console.log(req.body) // { name: 'new user', likes: ['coding'] }
+  console.log() // { name: 'new user', likes: ['coding'] }
   // logic for adding a new user
 })
