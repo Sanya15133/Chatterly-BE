0d4fd586f5a3dc1d0122be0a5c664a38811c4a68
Sanya Hussain <sanz_133@hotmail.com>
2024-06-30 15:51:03 +0100

    listened web socket

--- dist/src/listen.js
+++ dist/src/listen.js
@@ -18,3 +18,7 @@ try {
 catch (err) {
     console.error("Error in server setup:", err);
 }
+const server = app.listen(PORT, () => {
+    console.log(`IO Server is running on port ${PORT}`);
+});
+const io = require("socket.io")(server);
