f494f1582b2165be9df3e7238623372cd8592cf3
Sanya Hussain <sanz_133@hotmail.com>
2024-06-30 15:18:04 +0100

    added chat schema

--- dist/src/app.js
+++ dist/src/app.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
+const express = require("express");
 const connect_1 = __importDefault(require("./connect"));
 const app = express();
 app.use(express.json());
 (0, connect_1.default)();
-app.get('/api/users', (req, res, next) => {
+app.get("/api/users", (req, res, next) => {
     console.log(); // { name: 'new user', likes: ['coding'] }
     // logic for adding a new user
 });
--- dist/src/model/chat-model.js
+++ dist/src/model/chat-model.js
@@ -1,1 +1,23 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const chatSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: false,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+}, {
+    timestamps: true,
+});
+const Chat = (0, mongoose_1.model)("Chat", chatSchema);
+exports.default = Chat;
