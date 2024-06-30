f6697e6a447fa49411475616f1e8cb72a73f5ccf
Sanya Hussain <sanz_133@hotmail.com>
2024-06-30 17:41:55 +0100

    trying to get node working

--- src/src/app.ts
+++ src/src/app.ts
@@ -1,7 +1,10 @@
-const express = require("express");
+import express from 'express';
+import { createServer } from 'node:http';
 import connectMongoose from "./connect";
 
 const app = express();
+const server = createServer(app);
+
 
 app.use(express.json());
 
@@ -10,4 +13,12 @@ connectMongoose();
 app.get("/api/users", (req: any, res: any, next: any) => {
   console.log(); // { name: 'new user', likes: ['coding'] }
   // logic for adding a new user
+});
+
+app.get('/', (req: any, res: any) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
 });
