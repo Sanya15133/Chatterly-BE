f1c02da4e9ac9f749ac68ebf1733025077d9b792
Sanya Hussain <sanz_133@hotmail.com>
2024-07-01 18:03:09 +0100

    completed get chat route

--- dist/src/routes/chat-routes.js
+++ dist/src/routes/chat-routes.js
@@ -1,2 +1,7 @@
 "use strict";
-//chat
+Object.defineProperty(exports, "__esModule", { value: true });
+const chatRouter = require('express').Router();
+const Chat = require('../model/chat-model');
+const chat_controller_1 = require("../controllers/chat-controller");
+chatRouter.get('/', chat_controller_1.getChats);
+module.exports = chatRouter;
