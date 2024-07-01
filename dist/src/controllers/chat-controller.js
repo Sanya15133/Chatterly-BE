b5b7ca8d34833d51eedd26bd06ada1c3df36663a
Sanya Hussain <sanz_133@hotmail.com>
2024-07-01 18:00:35 +0100

    added get chat route

--- dist/src/controllers/chat-controller.js
+++ dist/src/controllers/chat-controller.js
@@ -1,1 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getChats = void 0;
+const chat_model_1 = require("../model/chat-model");
+function getChats(req, res, next) {
+    return (0, chat_model_1.findChat)()
+        .then((chats) => {
+        res.status(200).send({ chats });
+    })
+        .catch((error) => {
+        next(error);
+    });
+}
+exports.getChats = getChats;
