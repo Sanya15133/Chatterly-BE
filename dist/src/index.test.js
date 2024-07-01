ed339ae3edf05b61700b5316d8c6429a0765fe40
Sanya Hussain <sanz_133@hotmail.com>
2024-07-01 20:39:59 +0100

    added test file

--- dist/src/index.test.js
+++ dist/src/index.test.js
@@ -1,3 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_test_1 = require("node:test");
 const Model = require("./index");
 const expect = require("chai").expect;
+(0, node_test_1.describe)('Testing Endpoints', () => {
+    it('GET returns users in db', () => {
+    });
+});
