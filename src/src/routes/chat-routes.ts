ffb9f038ca5e7fea437872f6f09e842e33671e60
Sanya Hussain <sanz_133@hotmail.com>
2024-07-01 18:02:54 +0100

    completed get chat route

--- src/src/routes/chat-routes.ts
+++ src/src/routes/chat-routes.ts
@@ -1,1 +1,6 @@
-//chat
+const chatRouter = require('express').Router()
+const Chat = require('../model/chat-model')
+import { getChats } from "../controllers/chat-controller"
+
+chatRouter.get('/', getChats)
+module.exports = chatRouter
--- src/src/routes/user-routes.ts
+++ src/src/routes/user-routes.ts
@@ -2,7 +2,7 @@ const userRouter = require("express").Router();
 const User = require("../model/user-model");
 import { getUsers } from "../controllers/user-controller";
 
-userRouter.route("/:name");
+userRouter.route("/");
 
 userRouter.get("/users", getUsers);
 module.exports = userRouter;
