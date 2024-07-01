6b7f30aab7f76b691b895be1159c26295851665f
Sanya Hussain <sanz_133@hotmail.com>
2024-07-01 21:27:30 +0100

    first test done

--- src/src/index.test.ts
+++ src/src/index.test.ts
@@ -1,9 +1,8 @@
-const Model = require("./index");
 import { disconnectMongoose } from "./connect.js";
 import connectMongoose from "./connect.js";
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../app"); // replace with path to your server file
+const app = require("../src/index.js"); // replace with path to your server file
 chai.use(chaiHttp);
 const expect = chai.expect;
 
