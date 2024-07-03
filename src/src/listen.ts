980fa02cd40ee5b45971e54531257714bbb0f246
Sanya Hussain <sanz_133@hotmail.com>
2024-07-03 14:52:50 +0100

    sorting out listen file

--- src/src/listen.ts
+++ src/src/listen.ts
@@ -3,16 +3,6 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-try {
-  app
-    .listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    })
-    .on("error", (err: Error) => {
-      console.error("Error in server setup:", err);
-    });
-} catch (err) {
-  console.error("Error in server setup:", err);
-}
-
-
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
