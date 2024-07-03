8eeaf9a205084843c7ba491b90988788d64b70ef
Sanya Hussain <sanz_133@hotmail.com>
2024-07-03 13:32:40 +0100

    added post user

--- dist/src/routes/user-routes.js
+++ dist/src/routes/user-routes.js
@@ -5,4 +5,5 @@ const User = require("../model/user-model");
 const user_controller_1 = require("../controllers/user-controller");
 userRouter.get("/users", user_controller_1.getUsers);
 userRouter.get("/users/:name", user_controller_1.getUserByName);
+userRouter.post("/users", user_controller_1.insertUser);
 exports.default = userRouter;
