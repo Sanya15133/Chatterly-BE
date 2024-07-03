13b243f847bded8fe514bb909449dfbd10c5855c
Sanya Hussain <sanz_133@hotmail.com>
2024-07-03 14:58:50 +0100

    added socket.io

--- src/src/listen.ts
+++ src/src/listen.ts
@@ -1,8 +1,20 @@
 import express from "express";
-
+const http = require("http");
 const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
+  console.log(`APP is running on ${PORT}`);
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
+});
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
 });
