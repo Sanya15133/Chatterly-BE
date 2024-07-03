f3fe44939b8c3b205e0fb2f853fda9662c241ff0
Sanya Hussain <sanz_133@hotmail.com>
2024-07-03 11:59:49 +0100

    get chats by user model

--- src/src/controllers/chat-controller.ts
+++ src/src/controllers/chat-controller.ts
@@ -1,8 +1,19 @@
-import { findChats } from "../model/chat-model";
+import { findChats, findChatsByUser } from "../model/chat-model";
 
 export function getChats(req: any, res: any, next: any) {
   return findChats()
     .then((chats: any) => {
+      res.status(200).send({ chats });
+    })
+    .catch((error) => {
+      next(error);
+    });
+}
+
+export function getChatsByUser(req: any, res: any, next: any) {
+  const { name } = req.params;
+  return findChatsByUser(name)
+    .then((chats) => {
       res.status(200).send({ chats });
     })
     .catch((error) => {
