226a5534aa3c85f76ba1b616a7b1925d3d5b55db
Sanya Hussain <sanz_133@hotmail.com>
2024-07-03 13:41:10 +0100

    completed post chat

--- dist/src/controllers/chat-controller.js
+++ dist/src/controllers/chat-controller.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getChatsByUser = exports.getChats = void 0;
+exports.insertChats = exports.getChatsByUser = exports.getChats = void 0;
 const chat_model_1 = require("../model/chat-model");
 function getChats(req, res, next) {
     return (0, chat_model_1.findChats)()
@@ -23,3 +23,14 @@ function getChatsByUser(req, res, next) {
     });
 }
 exports.getChatsByUser = getChatsByUser;
+function insertChats(req, res, next) {
+    const { name, message, date } = req.body;
+    return (0, chat_model_1.addChat)(name, message, date)
+        .then((chat) => {
+        res.status(201).send({ chat });
+    })
+        .catch((error) => {
+        next(error);
+    });
+}
+exports.insertChats = insertChats;
