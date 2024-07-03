c1d0644fe6a4b211324e8538654fc23fabb6aa6e
Sanya Hussain <sanz_133@hotmail.com>
2024-07-03 12:06:38 +0100

    chat router sorted

--- dist/src/index.js
+++ dist/src/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const connect_js_1 = __importDefault(require("./connect.js"));
 const app_route_1 = __importDefault(require("./routes/app-route"));
 const user_routes_1 = __importDefault(require("./routes/user-routes"));
+const chat_routes_1 = __importDefault(require("./routes/chat-routes"));
 const app = (0, express_1.default)();
 const url = process.env.MONGODB_URI;
 app.use(express_1.default.json());
@@ -16,6 +17,10 @@ app.get("/api", app_route_1.default, (req, res, next) => {
     // logic for adding a new user
 });
 app.get("/users", user_routes_1.default, (req, res, next) => {
+    console.log(); // { name: 'new user', likes: ['coding'] }
+    // logic for adding a new user
+});
+app.get("/chats", chat_routes_1.default, (req, res, next) => {
     console.log(); // { name: 'new user', likes: ['coding'] }
     // logic for adding a new user
 });
--- dist/src/routes/chat-routes.js
+++ dist/src/routes/chat-routes.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const chatRouter = require("express").Router();
 const Chat = require("../model/chat-model");
 const chat_controller_1 = require("../controllers/chat-controller");
-chatRouter.get("/", chat_controller_1.getChats);
-module.exports = chatRouter;
+chatRouter.get("/chats", chat_controller_1.getChats);
+chatRouter.get("/chats/:name", chat_controller_1.getChatsByUser);
+exports.default = chatRouter;
