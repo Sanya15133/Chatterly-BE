404646a24c314d91b9a7fd19dda4211ca226fe1f
Sanya Hussain <sanz_133@hotmail.com>
2024-07-04 15:42:05 +0100

    added web socket server

--- src/src/listen.ts
+++ src/src/listen.ts
@@ -1,8 +1,24 @@
 import express from "express";
 const app = express();
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3000 });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`APP is running on ${PORT}`);
+});
+
+wss.on("connection", () => (ws: any) => {
+  console.log("Client connected");
+
+  ws.on("message", () => (message: any) => {
+    console.log("Received: %s", message);
+
+    ws.send(`${message}`);
+  });
+
+  ws.on("close", function () {
+    console.log("Client disconnected");
+  });
 });
