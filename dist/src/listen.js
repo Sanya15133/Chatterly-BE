3cd0e6cc3cb65cf7a1f7c6e51d14d2078564e381
Sanya Hussain <sanz_133@hotmail.com>
2024-07-04 15:42:17 +0100

    added web socket server

--- dist/src/listen.js
+++ dist/src/listen.js
@@ -5,7 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3000 });
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`APP is running on ${PORT}`);
+});
+wss.on("connection", () => (ws) => {
+    console.log("Client connected");
+    ws.on("message", () => (message) => {
+        console.log("Received: %s", message);
+        ws.send(`${message}`);
+    });
+    ws.on("close", function () {
+        console.log("Client disconnected");
+    });
 });
