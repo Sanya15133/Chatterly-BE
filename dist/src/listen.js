7fb3cd2ebed8c3bf27867f0c5126fd292f814bfe
Sanya Hussain <sanz_133@hotmail.com>
2024-07-04 15:34:43 +0100

    removed socket io

--- dist/src/listen.js
+++ dist/src/listen.js
@@ -4,18 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const http = require("http");
 const app = (0, express_1.default)();
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`APP is running on ${PORT}`);
-});
-server.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
-io.on("connection", (socket) => {
-    console.log("a user connected");
 });
