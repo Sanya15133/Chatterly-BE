4ffee69d7106fde04f5894b07748844614b1f93c
Sanya Hussain <sanz_133@hotmail.com>
2024-07-05 15:02:00 +0100

    added tests

--- src/src/__tests__/index.test.ts
+++ src/src/__tests__/index.test.ts
@@ -2,3 +2,11 @@ import request from "supertest";
 import connectMongoose from "../connect";
 import { disconnectMongoose } from "../connect";
 import app from "../index";
+
+beforeEach(() => {
+  connectMongoose();
+});
+
+afterEach(() => {
+  disconnectMongoose();
+});
