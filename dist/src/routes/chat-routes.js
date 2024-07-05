732fc59484ea794514e0e7afbeb1eb4047c2297f
Sanya Hussain <sanz_133@hotmail.com>
2024-07-05 21:41:48 +0100

    fixed router issue

--- dist/src/routes/chat-routes.js
+++ dist/src/routes/chat-routes.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const chatRouter = require("express").Router();
-const Chat = require("../model/chat-model");
 const chat_controller_1 = require("../controllers/chat-controller");
-chatRouter.get("/chats", chat_controller_1.getChats);
-chatRouter.get("/chats/:name", chat_controller_1.getChatsByUser);
+const chatRouter = require("express").Router();
+chatRouter.get("/", chat_controller_1.getChats);
+chatRouter.get("/:name", chat_controller_1.getChatsByUser);
 exports.default = chatRouter;
