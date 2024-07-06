19d2b487479eddd84950c78de721fd3e2150d215
Sanya Hussain <sanz_133@hotmail.com>
2024-07-06 08:46:12 +0100

    adding error handling

--- src/src/index.ts
+++ src/src/index.ts
@@ -2,6 +2,12 @@ import express from "express";
 import apiRouter from "./routes/app-route";
 import userRouter from "./routes/user-routes";
 import chatRouter from "./routes/chat-routes";
+const {
+  handleCustomErrors,
+  handle400Errors,
+  handle404Errors,
+  handle500Errors,
+} = require("./errors");
 
 const app = express();
 app.use(express.json());
@@ -9,5 +15,10 @@ app.use(express.json());
 app.use("/", apiRouter);
 app.use("/users", userRouter);
 app.use("/chats", chatRouter);
+
+app.use(handleCustomErrors);
+app.use(handle400Errors);
+app.use(handle404Errors);
+app.use(handle500Errors);
 
 export default app;
