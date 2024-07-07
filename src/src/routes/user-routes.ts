b28fba5741548a4d9d8ba817adb1a213d5ce43f1
Sanya Hussain <sanz_133@hotmail.com>
2024-07-07 20:18:12 +0100

    login function with jwt complete

--- src/src/routes/user-routes.ts
+++ src/src/routes/user-routes.ts
@@ -2,17 +2,14 @@ import {
   getUsers,
   getUserByName,
   insertUser,
+  loginUser,
 } from "../controllers/user-controller";
-
-
-
 
 const userRouter = require("express").Router();
 
 userRouter.get("/", getUsers);
 userRouter.get("/:name", getUserByName);
 userRouter.post("/", insertUser);
-// userRouter.post("/login", loginUser);
-
+userRouter.post("/login", loginUser);
 
 export default userRouter;
